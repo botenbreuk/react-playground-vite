88a180b79ebbadd69caa4d7e425921416929bc85
Robin <habo-robin@live.nl>
2023-03-11 22:45:51 +0100

    Updated dependencies and scss placement

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Routes from './app/Routes/Routes';
-import './styles/main.scss';
+import './assets/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
