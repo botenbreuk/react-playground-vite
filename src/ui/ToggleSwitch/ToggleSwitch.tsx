e53bb3915660021c1dc44424b92459ec91e3de0e
Robin de Boer <robin.de.boer@42.nl>
2024-07-18 11:35:29 +0200

    Added ToggleSwitch component

---
+++ src/ui/ToggleSwitch/ToggleSwitch.tsx
@@ -0,0 +1,21 @@
+import { omit } from 'lodash';
+import { FinalFieldProps } from '../types';
+
+type Props = {
+  label?: string;
+};
+
+export default function ToggleSwitch(
+  props: Omit<FinalFieldProps<Props, boolean>, 'value'>
+) {
+  const { label = '' } = props;
+  return (
+    <label className="toggle-switch">
+      <span className="switch">
+        <input {...omit(props, 'value')} type="checkbox" />
+        <span className="slider round" />
+      </span>
+      {label}
+    </label>
+  );
+}
