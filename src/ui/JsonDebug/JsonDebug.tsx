14c1128e8ec866d00c04bcd8fc956020455d2a3e
Robin de Boer <robin.de.boer@42.nl>
2024-12-04 09:56:11 +0100

    Added json debug component

---
+++ src/ui/JsonDebug/JsonDebug.tsx
@@ -0,0 +1,61 @@
+import classNames from 'classnames';
+import { useState } from 'react';
+import { Button } from '../Button/Button';
+import { Icon } from '../Icon';
+import './_json-debug.scss';
+
+type Props<T> = {
+  value: T;
+  alignment?: 'left' | 'right';
+  keys?: keyof T | (keyof T)[];
+  defaultOpen?: boolean;
+};
+
+export function JsonDebug<T>(props: Props<T>) {
+  const { value, alignment = 'left', keys, defaultOpen = false } = props;
+  const [show, setShow] = useState(defaultOpen);
+
+  function toggle() {
+    setShow(current => !current);
+  }
+
+  function getKeys() {
+    if (!keys) {
+      return {};
+    }
+
+    const obj = {} as Record<keyof T | string, any>;
+    if (Array.isArray(keys)) {
+      keys.forEach(v => {
+        obj[v] = value[v];
+      });
+    } else {
+      obj[keys] = value[keys];
+    }
+    return obj;
+  }
+
+  if (import.meta.env.MODE !== 'development') {
+    return null;
+  }
+
+  if (!show) {
+    return (
+      <Button className="json-debug-overlay-toggle" onClick={toggle}>
+        Open JSON
+      </Button>
+    );
+  }
+
+  return (
+    <>
+      <Button className="json-debug-overlay-toggle" onClick={toggle}>
+        Open JSON
+      </Button>
+      <div className={classNames('json-debug-overlay', alignment)}>
+        <pre>{JSON.stringify(keys ? getKeys() : value, null, 2)}</pre>
+        <Icon type="icon-cross" onClick={toggle} />
+      </div>
+    </>
+  );
+}
