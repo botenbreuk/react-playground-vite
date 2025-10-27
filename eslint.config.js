689ee3137689f925830b30600aecf42ae61798a0
Robin de Boer <robin.de.boer@42.nl>
2025-10-27 14:19:13 +0100

    Changed deprecated tseslint.config to eslint/config defineConfig

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,10 @@ import pluginCheckFile from 'eslint-plugin-check-file';
 import pluginImport from 'eslint-plugin-import';
 import pluginReact from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
+import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default defineConfig(
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat['jsx-runtime'],
